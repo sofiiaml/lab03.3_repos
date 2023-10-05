81d27ba9464db88b96cf228c9ae8af6bca65df40
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-10-05 21:34:59 +0300

    first master commit

---
+++ PR3.3/PR3.3.cpp
@@ -0,0 +1,37 @@
+﻿// Lab_03_3.cpp
+// < Молотюк Софія >
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 16.
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double R1; // вхідний параметр
+	double R2; // вхідний параметр
+	double y; // результат обчислення виразу
+	cout << "R1 = "; cin >> R1;
+	cout << "R2 = "; cin >> R2;
+	cout << "x = "; cin >> x;
+	// розгалуження в повній формі
+	if (x <= -R1)
+		y = -R1 * (x + 2) / (-R1 + 2);
+	else
+		if (-R1 < x && x <= 0)
+			y = -R1 + sqrt(R1 * R1 - x * x);
+		else
+			if (0 < x && x <= R2)
+				y = R2 - sqrt(R2 * R2 - x * x);
+			else
+				if (R2 < x && x <= 4)
+					y = -R1;
+				else
+					y = R1 * (x - 6) / 2;
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+}
+
